22b41bca926f0bc9ef9daa69b6ae8c7361e17658
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-01-22 22:40:42 +0600

    Create rectangle.cpp

---
+++ contest 4/rectangle.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i;
+   for(i=1;i<=n;i++) {
+        int x,y,z;
+        cin>>x>>y>>z;
+        if((x>20)||(y>20)||(z>20)){
+
+            cout<<"Case "<<i<<": "<<"bad"<<endl;
+        }
+        else
+            cout<<"Case "<<i<<": "<<"good"<<endl;
+
+
+
+    }
+
+
+
+
+
+}
